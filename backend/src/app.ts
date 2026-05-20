67fa0ee6290792423b83d22e6b6f5cb7a0d7975a
Madan <29ramadhanirachman@gmail.com>
2026-05-20 22:00:15 +0700

    add booking backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,28 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import authRoutes from './routes/auth.route.js';
-
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/auth.route.js";
+import { startBookingCron } from "./cron/cancelExpiredBookings.js";
+import bookingRoutes from "./routes/booking.route.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-
-app.use((err: Error, _req: express.Request, res: express.Response, _next: express.NextFunction) => {
-  res.status(500).json({ error: 'Internal Server Error' });
-});
+startBookingCron();
+app.use("/api/auth", authRoutes);
+app.use("/api/bookings", bookingRoutes);
+app.use(
+  (
+    err: Error,
+    _req: express.Request,
+    res: express.Response,
+    _next: express.NextFunction,
+  ) => {
+    res.status(500).json({ error: "Internal Server Error" });
+  },
+);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
